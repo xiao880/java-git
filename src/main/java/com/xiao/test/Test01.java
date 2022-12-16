f69379bd2216ca3ac5a6c9f5b9346969285bb261
xiao880 <583228547@qq.com>
2022-12-16 17:45:15 +0800

    commit 3

--- src/main/java/com/xiao/test/Test01.java
+++ src/main/java/com/xiao/test/Test01.java
@@ -4,6 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("heihei , idea git");
         System.out.println("heihei , idea git2");
-
+        System.out.println("heihei , idea git3");
     }
 }
